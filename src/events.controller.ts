6cb511da8d763a79c2627fac7235bec2a8236ba2
Thomas Hodges <thodges@javascriptonline.org>
2023-12-30 22:19:21 -0800

    responses and status codes

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
@@ -11,17 +12,40 @@ import {
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      {
+        id: 1,
+        title: 'Event 1',
+        description: 'Description 1',
+        date: new Date(),
+      },
+      {
+        id: 2,
+        title: 'Event 2',
+        description: 'Description 2',
+        date: new Date(),
+      },
+    ];
+  }
   @Get('/:id')
   findOne(@Param('id') id) {
-    return `This action returns a #${id} event`;
+    return {
+      id: id,
+      title: `Event ${id}`,
+      description: `Description ${id}`,
+      date: new Date(),
+    };
   }
   @Post()
   create(@Body() input) {
     return input;
   }
   @Patch('/:id')
-  update(@Param('id') id, @Body() input) {}
+  update(@Param('id') id, @Body() input) {
+    return input;
+  }
   @Delete('/:id')
+  @HttpCode(204)
   remove(@Param('id') id) {}
 }
